7cd913298184f59d8020e09b395b572475468b1c
tkent97 <tristen123abc@gmail.com>
2021-09-07 00:42:59 -0700

    initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import react from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello dojo</h1>
+      <h2>things i need to do:</h2>
+      <ul>
+        <li>learn react</li>
+        <li>climb mt.everest</li>
+        <li>run a marathon</li>
+        <li>feed the dogs</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
